9dd786cf3c2cb5efc7405e3169a09e4da704aa12
Coronado, Benjamin (Student) <bc245@students.waikato.ac.nz>
2024-07-25 14:56:46 +1200

    Create DetailActivity.java and activity_detail.xml

--- app/src/main/java/com/example/assignmentone/DetailActivity.java
+++ app/src/main/java/com/example/assignmentone/DetailActivity.java
@@ -1,9 +1,6 @@
 package com.example.assignmentone;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detail);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-
-    public void onclickIcon(View v) {
-        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this,DetailActivity.class);
-        startActivity(intent);
     }
 }
