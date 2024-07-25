344f0854bdab6c0a6e498c87fabbc4c868721e86
Coronado, Benjamin (Student) <bc245@students.waikato.ac.nz>
2024-07-25 13:31:51 +1200

    Add Toast onclick event on icons

--- app/src/main/java/com/example/assignmentone/MainActivity.java
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.assignmentone;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +24,7 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+        public void onclickIcon(View v) {
+            Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+        }
 }
